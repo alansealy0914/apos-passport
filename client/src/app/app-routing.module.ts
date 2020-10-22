60f13c8475c3f9e8cbf8546a0ec031a0d7b03c68
alan_sealy <alan.sealy@gmail.com>
2020-10-22 14:22:51 -0500

    refactored

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -4,13 +4,15 @@ import { HomeComponent } from "./home/home.component";
 import { LoginComponent } from "./login/login.component";
 import { RegisterComponent } from "./register/register.component";
 import { ProfileComponent } from "./profile/profile.component";
+
 import { AuthGuard } from "./auth.guard";
 
 const routes: Routes = [
   { path: "", component: HomeComponent },
   { path: "login", component: LoginComponent },
   { path: "register", component: RegisterComponent },
-  { path: "profile", component: ProfileComponent, canActivate: [AuthGuard] }
+  { path: "profile", component: ProfileComponent, canActivate: [AuthGuard] },
+  
 ];
 
 @NgModule({
