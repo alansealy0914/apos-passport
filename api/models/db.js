bdbd89b79673e7a9cfd27e0ec6b618919215c9cb
alan_sealy <alan.sealy@gmail.com>
2020-10-22 19:58:17 -0500

    updated mongo DB

--- api/models/db.js
+++ api/models/db.js
@@ -1,6 +1,6 @@
 require('./users');
 const mongoose = require('mongoose');
-const dbURI = 'mongodb://localhost:27017/meanAuth';
+const dbURI = 'mongodb://localhost:27017/mean';
 //const dbURI = 'mongodb://localhost:27017/aposDB';
 
 mongoose.set('useCreateIndex', true);
